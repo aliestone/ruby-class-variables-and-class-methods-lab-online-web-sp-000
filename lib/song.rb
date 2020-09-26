3ae99e228025601a29666ee0c9a61ef57faaeefd
Alie Stone <helloaliestone@gmail.com>
2020-09-26 21:10:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,25 +1,41 @@
+
+
 class Song
-@@count = 0
 
-def initialize(name, artist, genres)
-  @@count += 1
-end
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
 
-def name=(name)
-  @name = name
-end
+  def initialize(song_name,artist,genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
 
-def name
-  @name
-end 
+  def self.count
+    @@count
+  end
 
+  def self.artists
+    @@artists.uniq
+  end
 
-def self.count
-  @@count
-end
+  def self.genres
+    @@genres.uniq
+  end
 
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
 
-
-
-
-end
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    artist_count
+  end
