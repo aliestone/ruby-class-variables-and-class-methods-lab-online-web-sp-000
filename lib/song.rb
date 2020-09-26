443cf93d6f772e0405937aea7cc9d3a04d46c726
Alie Stone <helloaliestone@gmail.com>
2020-09-26 20:57:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,10 @@ class Song
 
 @@count = 0
 def initialize(name, artist, genres)
-  @@artists = artist 
+  @@artists = artist
   @@genres = genres
   @@count = count
-  @@count += 1 
+  @@count += 1
 end
 
 def count(number)
